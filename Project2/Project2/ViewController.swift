f3a3695922ea59e9091ebc0ab4c77532b8725d1e
Spencer Jones <spencerjones@fnni.com>
2022-01-28 23:24:19 -0600

    Start project2 Add title and button borders

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Spencer Jones on 1/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "united kingdom", "united states"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 2
+        button3.layer.borderWidth = 3
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+        
+        
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
+    }
+
+
+}
+
